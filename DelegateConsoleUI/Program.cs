caf6248c4ff02c6bd0fca0bd9a4c195af0b1a9e4
Cody Siegmann <codysiegmann09@gmail.com>
2021-09-15 14:21:00 -0700

    c# delegates

---
+++ DelegateConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using DelegatesModel;
+using System;
+
+namespace DelegateConsoleUI
+{
+    public class Program
+    {
+
+        static ShoppingCartModel cart = new ShoppingCartModel();
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal():C2}");
+
+            Console.WriteLine();
+            Console.Write("Please enter any key to exit the application.");
+            Console.ReadKey();
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+    }
+}
---
+++ DelegatesModel/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DelegatesModel
+{
+    public class ProductModel
+    {
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
